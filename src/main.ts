d36ab1b766ed9e898de5625cc1ce060f8445daeb
wangxingren <xingren.wang@huixiaoer.com>
2022-02-16 17:48:22 +0800

    fix:eslit

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@
  * @Author: wangxingren
  * @Date: 2022-02-07 17:32:53
  * @LastEditors: wangxingren
- * @LastEditTime: 2022-02-16 17:33:04
+ * @LastEditTime: 2022-02-16 17:47:41
  * @Descripttion: main.js
  * @FilePath: /vue3-avator-ts/src/main.ts
  */
 import { createApp } from "vue";
-import App from './App.vue'
+import App from "./App.vue"
 
 createApp(App).mount("#app");
